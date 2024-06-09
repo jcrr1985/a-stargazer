d6f93f8d2d6981a67278d7881595e7de95b205ce
julio rodriguez <julio.rodriguez-ext@talan.com>
2024-06-10 01:20:40 +0500

    changing api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { LoggerConfig } from 'ngx-logger/lib/logger.config';
 
 export const environment = {
   production: false,
-  baseUrl: 'https://stargazer-server.onrender.com',
+  baseUrl: 'https://cars-server-1.onrender.com',
   logger: {
     serverLoggingUrl: undefined,
     level: NgxLoggerLevel.DEBUG,
