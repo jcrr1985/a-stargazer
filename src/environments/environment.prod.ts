a91c7659ccf98660e59a9351cbc2573448aa67ed
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-09-20 00:41:34 +0500

    removing extra slash in baseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { LoggerConfig } from 'ngx-logger/lib/logger.config';
 
 export const environment = {
   production: false,
-  baseUrl: 'https://1f1d-82-215-116-48.ngrok-free.app/',
+  baseUrl: 'https://1f1d-82-215-116-48.ngrok-free.app',
   logger: {
     serverLoggingUrl: undefined,
     level: NgxLoggerLevel.DEBUG,
